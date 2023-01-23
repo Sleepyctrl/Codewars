3af7648fb2c599f42eed2153bee2c634a9be0ea5
Sleepyctrl <88786733+Sleepyctrl@users.noreply.github.com>
2023-01-24 00:55:50 +0300

    Refactor+add unit test

---
+++ main/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text.RegularExpressions;
+
+namespace Codewars
+{
+    public class Task1
+    {
+        public static string ReverseAndOmmit_REGEX(string str)
+        {
+            Regex regex = new Regex(@"[^A-Z]", RegexOptions.IgnoreCase);
+            string s = regex.Replace(str, "");
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        public static string ReverseAndOmmit_LINQ(string str)
+        {
+            return new string(str.Where(Char.IsLetter).Reverse().ToArray());
+        }
+
+    }
+    public class Task2
+    {
+        public static int Cats_EasyAlgorithm(int start, int finish)
+        {
+            int count = 0;
+            for (int i = start; i < finish;)
+            {
+                if (i + 3 > finish)
+                {
+                    i++; count++;
+                }
+                else
+                {
+                    i += 3;
+                    count++;
+                };
+            }
+            return count;
+        }
+    }
+    public class Task3
+    {
+        public static string  FirstLetterWordToUp_Regex(string phrase)
+        {
+            var secondQuery = Regex.Replace(phrase, /*В первом критерии ищем
+                                                     первый символ строки \w и во втором условии
+                                                     ищем пробел и первый символ строки*/@"(^\w)|(\s\w)", m => m.Value.ToUpper());
+            return secondQuery;
+        }
+        public static string FirstLetterWordToUp_LINQ(string phrase)
+        {
+            return phrase != "" ? String.Join(" ", phrase.Split().Select(i => Char.ToUpper(i[0]) + i.Substring(1))) : "";
+        }
+    }
+    public class Task4
+    {
+        public static int Find_the_odd_int_that_appears_an_odd_time_LINQ(int[] arr)
+        {
+            var arrcopy = arr.GroupBy(x => x).Where(x => x.Count() % 2 != 0);
+            return arrcopy.Count()>=1? arrcopy.First().Key : 0;
+        }
+    }
+    public class Task5
+    {
+        public static int Numbers_of_time_multiply_until_single_digit_Recursion(int number)
+        {
+            for (int i = 0; ; i++)
+            {
+                if (number < 10) return i;
+                number = sol(number);
+            }
+        }
+        public static int sol(int n)
+        {
+            if (n < 10) return (int)n;
+            else return (int)(n % 10) * sol(n / 10);
+        }
+    }
+    public class Task6
+    {
+        public static string Convert_first_char_into_title_case_with_except_Regex(string mainstring, string? filterstring = "")
+        {
+            //Первая буква первого слова является началом предложения, потому всегда пишется с заглавной буквы
+            string[] Filter = (filterstring != null) ? filterstring.ToLower().Split(" ") : "".ToLower().Split(" ");
+            string[] Main = mainstring.ToLower().Split(" ");
+            string Result = "";
+            foreach (string s in Main)
+            {
+                if (Filter.Contains(s)) Result += s + " ";
+                else Result += Regex.Replace(s, @"(^\w)", m => m.Value.ToUpper()) + " ";
+            }
+            return Regex.Replace(Result, @"(^\w)", m => m.Value.ToUpper()).Remove(Result.Length - 1);
+
+        }
+    }
+    public class Task7
+    {
+        public static bool Is_integer_prime(int n)
+        {
+            if (n == 2) return true;
+            if (n == 0 || n == 1 || n < 0 || n % 2 == 0) return false;
+            var boundary = (int)Math.Sqrt(n);
+            for (int i = 3; i <= boundary; i+=2)
+            {
+                if (n % i == 0 && i != n) return false;
+            }
+            return true;
+        }
+    }
+    public class Task8
+    {
+        public static int[] Move_Zeros_to_end(int[] arr)
+        {
+            //Kata.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
+            return arr.Where(x => x != 0).Concat(arr.Where(x => x == 0)).ToArray();
+        }
+
+    }
+    public class Task9
+    {
+        public static int[] TASK9(int[] Numbers, int Target)
+        {
+            //two_sum([1, 2, 3], 4) == {0, 2}
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                for (int b = 1; b < Numbers.Length; b++)
+                {
+                    if (Numbers[i] + Numbers[b] == Target && i != b)
+                    {
+                        return new int[] { i, b };
+                    }
+                }
+            }
+            // public static int[] TwoSum(int[] numbers, int target) =>
+            // numbers.Select((x,i)=>new [] {i, Array.IndexOf(numbers,target-x,i+1)})
+            // .First(x => x[1] != -1);
+            return new int[0];
+        }
+    }
+    public class Task10
+    {
+        public static bool TASK10(string str)
+        {
+            return str == "" ? false : !new Regex(@"[\W]|[._]").IsMatch(str);
+        }
+    }
+    public class Task11
+    {
+        public static string TASK11(int[] args)
+        {
+            int count;
+            //string builder
+            List<string> list = new List<string>();
+            // solution([-10, -9, -8, -6, -4, -5, -1, 0, 1, 3, 4, 5, 7, 8, 9, 11, 12, 14, 15, 17, 19, 20, 21]);
+            //{ -10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 }
+            // returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+            for (int i = 0; i <= args.Length; i++)
+            {
+                count = 1;
+                for (int g = i + 1; g <= args.Length; g++)
+                {
+                    if (g == args.Length && count >= 3)
+                    {
+                        list.Add($"{args[g - count]}-{args[g - 1]}");
+                        return String.Join(",", list);
+                    }
+
+                    if (g != args.Length && args[i] + 1 == args[g])
+                    {
+                        count++; i++;
+                    }
+                    else
+                    {
+                        if (count >= 3)
+                        {
+                            list.Add($"{args[g - count]}-{args[g - 1]}");
+                        }
+                        else
+                        {
+
+                            if (count == 2)
+                            {
+                                list.Add($"{args[i - 1]}");
+                                list.Add($"{args[i]}");
+                            }
+                            else
+                            {
+                                list.Add($"{args[i]}");
+                            }
+
+                        }
+
+                        break;
+                    }
+
+                }
+            }
+            return String.Join(",", list);
+
+
+        }
+    }
+        
+       
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ test/Task_codewarsTests.cs
@@ -0,0 +1,447 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codewars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.Tests
+{
+    [TestClass()]
+    public class Task1Tests
+    {
+        [TestMethod()]
+        public void Task1ReverseAndOmmitRegex_SomeWord_ReturnReverseWord()
+        {
+            //Arrange
+            string input = "krishan";
+            string expected = "nahsirk";
+
+            //Act
+            string actual = Task1.ReverseAndOmmit_REGEX(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task1ReverseAndOmmitRegex_SomeWordAndNumbers_ReturnReverseWordAndOmmitNonAlhpabet()
+        {
+            //Arrange
+            string input = "ultr53o?n ";
+            string expected = "nortlu";
+
+            //Act
+            string actual = Task1.ReverseAndOmmit_REGEX(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task1ReverseAndOmmitRegex_EmptyString_Return()
+        {
+            //Arrange
+            string input = "";
+            string expected = "";
+            //Act
+            string actual = Task1.ReverseAndOmmit_REGEX(input);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Test Task1.ReverseAndOmmit_LINQ
+
+        [TestMethod()]
+        public void Task1ReverseAndOmmitLINQ_SomeWord_ReturnReverseWord()
+        {
+            //Arrange
+            string input = "5332";
+            string expected = "";
+
+            //Act
+            string actual = Task1.ReverseAndOmmit_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task1ReverseAndOmmitLINQ_SomeWordAndNumbers_ReturnReverseWordAndOmmitNonAlhpabet()
+        {
+            //Arrange
+            string input = "ultr53o?n ";
+            string expected = "nortlu";
+
+            //Act
+            string actual = Task1.ReverseAndOmmit_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Task1ReverseAndOmmitLINQ_EmptyString_ReturnEmptyString()
+        {
+            //Arrange
+            string input = "";
+            string expected = "";
+            //Act
+            string actual = Task1.ReverseAndOmmit_LINQ(input);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass()]
+    public class Task2Tests
+    {
+        [TestMethod()]
+        public void Cats_EasyAlgorithm_1and5_Return2()
+        {
+            //Arrange
+            int Start = 1;
+            int Finish = 5;
+            int expected = 2;
+
+            //Act
+            int actual = Task2.Cats_EasyAlgorithm(Start, Finish);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Cats_EasyAlgorithm_12and4_Return0()
+        {
+            //Arrange
+            int Start = 12;
+            int Finish = 4;
+            int expected = 0;
+
+            //Act
+            int actual = Task2.Cats_EasyAlgorithm(Start, Finish);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass()]
+    public class Task3Tests
+    {
+        [TestMethod()]
+        public void FirstLetterWordToUpRegex_should_return_capitalized_words_on_valid_input()
+        {
+            //Arrange
+            string input = "How can mirrors be real if our eyes aren't real";
+            string expected = "How Can Mirrors Be Real If Our Eyes Aren't Real";
+
+            //Act
+            string actual = Task3.FirstLetterWordToUp_Regex(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void FirstLetterWordToUpRegex_should_return_empty_string_on_empty_input()
+        {
+            //Arrange 
+            string input = "";
+            string expected = "";
+
+            //Act
+            string actual = Task3.FirstLetterWordToUp_Regex(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void FirstLetterWordToUpLinq_should_return_capitalized_words_on_valid_input()
+        {
+            //Arrange
+            string input = "How can mirrors be real if our eyes aren't real";
+            string expected = "How Can Mirrors Be Real If Our Eyes Aren't Real";
+
+            //Act
+            string actual = Task3.FirstLetterWordToUp_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void FirstLetterWordToUpLinq_should_return_empty_string_on_empty_input()
+        {
+            //Arrange 
+            string input = "";
+            string expected = "";
+
+            //Act
+            string actual = Task3.FirstLetterWordToUp_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+
+    [TestClass()]
+    public class Task4Tests
+    {
+        [TestMethod()]
+        public void Find_the_odd_int_that_appears_an_odd_time_LINQ_should_return_4_on_valid_array()
+        {
+            //Arrange 
+            int[] input = new int[] { 1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1 };
+            int expected = 4;
+
+            //Act
+            int actual = Task4.Find_the_odd_int_that_appears_an_odd_time_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void Find_the_odd_int_that_appears_an_odd_time_LINQ_should_return_2_on_valid_array()
+        {
+            //Arrange 
+            int[] input = new int[] { 1, 1, 2 };
+            int expected = 2;
+
+            //Act
+            int actual = Task4.Find_the_odd_int_that_appears_an_odd_time_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Find_the_odd_int_that_appears_an_odd_time_LINQ_should_return_0_on_valid_array()
+        {
+            //Arrange
+            int[] input = new int[] { 0 };
+            int expected = 0;
+
+            //Act
+            int actual = Task4.Find_the_odd_int_that_appears_an_odd_time_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Find_the_odd_int_that_appears_an_odd_time_LINQ_should_return_0_on_invalid_array()
+        {
+            //Arrange
+            int[] input = new int[] { };
+            int expected = 0;
+
+            //Act
+            int actual = Task4.Find_the_odd_int_that_appears_an_odd_time_LINQ(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass()]
+    public class Task5Tests
+    {
+        [TestMethod()]
+        public void Numbers_of_time_multiply_until_single_digit_Recursion_should_return_4_on_valid_int()
+        {
+            //Arrange
+            int input = 999;
+            int expected = 4;
+
+            //Act
+            int actual = Task5.Numbers_of_time_multiply_until_single_digit_Recursion(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void Numbers_of_time_multiply_until_single_digit_Recursion_should_return_0_on_valid_int()
+        {
+            //Arrange
+            int input = 4;
+            int expected = 0;
+
+            //Act
+            int actual = Task5.Numbers_of_time_multiply_until_single_digit_Recursion(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void Numbers_of_time_multiply_until_single_digit_Recursion_should_return_0_on_invalid_int()
+        {
+            //Arrange
+            int Input = -55;
+            int expected = 0;
+
+            //Act
+            int actual = Task5.Numbers_of_time_multiply_until_single_digit_Recursion(Input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+
+    [TestClass()]
+    public class Task6Tests
+    {
+        [TestMethod()]
+        public void Convert_first_char_into_title_case_with_except_Regex_should_return_convertString_on_valid_input_with_filter()
+        {
+            //Arrange
+            string maininput = "a clash of KINGS";
+            string filterinput = "a an the of";
+            string expected = "A Clash of Kings";
+
+            //Act
+            string actual = Task6.Convert_first_char_into_title_case_with_except_Regex(maininput, filterinput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Convert_first_char_into_title_case_with_except_Regex_should_return_convertString_on_valid_input_without_filter()
+        {
+            //Arrange
+            string maininput = "the quick brown fox";
+            // string filterinput = "The In";
+            string expected = "The Quick Brown Fox";
+
+            //Act
+            string actual = Task6.Convert_first_char_into_title_case_with_except_Regex(maininput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Convert_first_char_into_title_case_with_except_Regex_should_return_convertString_on_invalid_input_with_filter()
+        {
+            //Arrange
+            string maininput = "";
+            string filterinput = "";
+            string expected = "";
+
+            //Act
+            string actual = Task6.Convert_first_char_into_title_case_with_except_Regex(maininput, filterinput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass]
+    public class Task7Tests
+    {
+        [TestMethod()]
+        public void Is_integer_prime_should_return_true_on_input_11()
+        {
+            //Arrange
+            int input = 2;
+            bool expected = true;
+
+            //Act
+            bool actual = Task7.Is_integer_prime(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Is_integer_prime_should_return_false_on_input_0()
+        {
+            //Arrange
+            int input = 0;
+            bool expected = false;
+
+            //Act
+            bool actual = Task7.Is_integer_prime(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Is_integer_prime_should_return_false_on_input_negative_number()
+        {
+            //Arrange
+            int input = -57;
+            bool expected = false;
+
+            //Act
+            bool actual = Task7.Is_integer_prime(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass]
+    public class Task8Tests
+    {
+        [TestMethod()]
+        public void Move_Zeros_to_end_should_return_sorted_array_on_valid_input()
+        {
+            //Arrange
+            int[] input = new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 };
+            int[] expected = new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 };
+
+            //Act
+            int[] actual = Task8.Move_Zeros_to_end(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Move_Zeros_to_end_should_return_nosorted_array_on_input_without_0()
+        {
+            //Arrange
+            int[] input = new int[] { 1, 2, 1, 1, 3, 1 };
+            int[] expected = new int[] { 1, 2, 1, 1, 3, 1 };
+
+            //Act
+            int[] actual = Task8.Move_Zeros_to_end(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Move_Zeros_to_end_should_return_nosorted_array_on_input_without_0_and_with_negative()
+        {
+            //Arrange
+            int[] input = new int[] { 1, 2, 1, 1, 3, 1,-1,-3,-2 };
+            int[] expected = new int[] { 1, 2, 1, 1, 3, 1, -1, -3, -2};
+
+            //Act
+            int[] actual = Task8.Move_Zeros_to_end(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Move_Zeros_to_end_should_return_empty_array_on_input_empty_array()
+        {
+            //Arrange
+            int[] input = new int[] {};
+            int[] expected = new int[] {};
+
+            //Act
+            int[] actual = Task8.Move_Zeros_to_end(input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
